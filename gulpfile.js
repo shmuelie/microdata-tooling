be9425390040498b6f07ed6fc85d862b487a473d
Shmueli Englard <shmueli.yosef@englard.net>
2021-02-07 12:45:00 -0500

    I give up on multi targeting

--- gulpfile.js
+++ gulpfile.js
@@ -7,52 +7,27 @@ import merge from 'merge2'
 import fs from 'fs'
 import fsa from 'fs/promises'
 
-const mainDest = "dist";
-/**
- * @type {{target: string, dest: string}[]}
- */
-const configs = JSON.parse(fs.readFileSync("configurations.json").toString());
-
-/**
- * @type {string[]}
- */
-const tsBuilds = [
-];
-/**
- * @type {string[]}
- */
-const cleanBuilds = [];
-for (const config of configs) {
-    const tsName = "ts-build-" + config.target;
-    gulp.task(tsName, function () {
-        const tsProject = ts.createProject("tsconfig.json", {
-            target: config.target
-        });
-        const result = tsProject.src().pipe(sourcemaps.init()).pipe(tsProject());
-        return merge([
-            result.js.pipe(terser()).pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: "../src" })).pipe(gulp.dest(config.dest)),
-            result.dts.pipe(gulp.dest(config.dest))
-        ]);
-    });
-    tsBuilds.push(tsName);
-    const cleanName = "clean-" + config.target;
-    gulp.task(cleanName, async function () {
-        /**
-         * @type {false | fs.Stats}
-         */
-        const stat = await fsa.stat(config.dest).catch(/** @param {NodeJS.ErrnoException | null} err */(err) => {
-            if (err && err.code === "ENOENT") {
-                return false;
-            }
-            throw err;
-        });
-        if (stat && stat.isDirectory()) {
-            await del(config.dest + "/*.*");
+gulp.task("ts-build", function () {
+    const tsProject = ts.createProject("tsconfig.json");
+    const result = tsProject.src().pipe(sourcemaps.init()).pipe(tsProject());
+    return merge([
+        result.js.pipe(terser()).pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: "../src" })).pipe(gulp.dest("dist")),
+        result.dts.pipe(gulp.dest("dist"))
+    ]);
+});
+gulp.task("clean", async function () {
+    /**
+     * @type {false | fs.Stats}
+     */
+    const stat = await fsa.stat("dist").catch(/** @param {NodeJS.ErrnoException | null} err */(err) => {
+        if (err && err.code === "ENOENT") {
+            return false;
         }
+        throw err;
     });
-    cleanBuilds.push(cleanName);
-}
+    if (stat && stat.isDirectory()) {
+        await del("dist/*.*");
+    }
+});
 
-gulp.task("ts-build", gulp.series(tsBuilds));
-gulp.task("clean", gulp.series(cleanBuilds));
 gulp.task("build", gulp.series(["clean", "ts-build"]));
