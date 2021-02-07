df322dbe8a284c92e2fce58436e52e4dff6a194e
Shmueli Englard <shmueli.yosef@englard.net>
2021-02-07 01:16:25 -0500

    Added support for building multiple targets

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,54 @@ import sourcemaps from 'gulp-sourcemaps'
 import terser from 'gulp-terser'
 import del from 'del'
 import merge from 'merge2'
+import fs from 'fs'
+import fsa from 'fs/promises'
 
-const tsProject = ts.createProject("tsconfig.json");
+const mainDest = "dist";
+/**
+ * @type {{target: string, dest: string}[]}
+ */
+const configs = JSON.parse(fs.readFileSync("configurations.json").toString());
 
-gulp.task("clean", function () {
-    return del("dist/*.*");
-});
 
-gulp.task("ts-build", function () {
-    const js = tsProject.src().pipe(sourcemaps.init()).pipe(tsProject());
-    return merge([
-        js.js.
-        pipe(terser()).
-        pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: "../src" })).pipe(gulp.dest("dist")),
-        js.dts.pipe(gulp.dest("dist"))
-    ]);
-});
 
+/**
+ * @type {string[]}
+ */
+const tsBuilds = [
+];
+/**
+ * @type {string[]}
+ */
+const cleanBuilds = [];
+for (const config of configs) {
+    const tsName = "ts-build-" + config.target;
+    gulp.task(tsName, function () {
+        const tsProject = ts.createProject("tsconfig.json", {
+            target: config.target
+        });
+        const result = tsProject.src().pipe(sourcemaps.init()).pipe(tsProject());
+        return merge([
+            result.js.pipe(terser()).pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: "../src" })).pipe(gulp.dest(config.dest)),
+            result.dts.pipe(gulp.dest(config.dest))
+        ]);
+    });
+    tsBuilds.push(tsName);
+    const cleanName = "clean-" + config.target;
+    gulp.task(cleanName, async function () {
+        const stat = await fsa.stat(config.dest).catch(err => {
+            if (err && err.code === "ENOENT") {
+                return false;
+            }
+            throw err;
+        });
+        if (stat && stat.isDirectory()) {
+            await del(config.dest + "/*.*");
+        }
+    });
+    cleanBuilds.push(cleanName);
+}
+
+gulp.task("ts-build", gulp.series(tsBuilds));
+gulp.task("clean", gulp.series(cleanBuilds));
 gulp.task("build", gulp.series(["clean", "ts-build"]));
