5794702e491d0eec7be5393a52144e3c05dd79d4
Shmueli Englard <shmueli.yosef@englard.net>
2021-02-07 23:15:08 -0500

    Added parse to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Thing } from './schema-simple.js'
 export { MicroJson, MicroJsonThing, jsonify } from './jsonify.js'
 export { apply, ApplyOptions } from './apply.js'
+export { parse, ParseOptions } from './parse.js'
