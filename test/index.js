933ba3ca05369e51c68816b8a9a6359dbab81a5c
Shmueli Englard <shmueli.yosef@englard.net>
2021-02-08 21:01:18 -0500

    Added test for parse

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 import assert from 'assert'
 import JSDOM from 'jsdom'
-import { jsonify } from '../dist/index.js'
+import { jsonify, parse } from '../dist/index.js'
 import fs from 'fs/promises'
 
 suite("microdata-tooling", function () {
@@ -11,6 +11,16 @@ suite("microdata-tooling", function () {
         const document = dom.window.document;
         const actual = jsonify(Array.from(document.body.children));
         const expected = JSON.parse((await fs.readFile("test/jsonify.json")).toString());
+        assert.deepStrictEqual(actual, expected);
+    });
+
+    test("parse", async function () {
+        const dom = await JSDOM.JSDOM.fromFile("test/jsonify.htm", {
+            url: "https://blog.example.com/progress-report"
+        });
+        const document = dom.window.document;
+        const actual = parse(document.getElementsByTagName("html")[0]);
+        const expected = JSON.parse((await fs.readFile("test/parse.json")).toString());
         assert.deepStrictEqual(actual, expected);
     });
 })
