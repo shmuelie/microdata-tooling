cb2baddb89e865861b8d52928a32dc8e3ff7d65d
Shmueli Englard <shmueli.yosef@englard.net>
2021-02-15 14:35:34 -0500

    Changed to use gulp-rewrite-imports

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,18 @@ import terser from 'gulp-terser'
 import ts from 'gulp-typescript'
 import typedoc from 'gulp-typedoc'
 import fsa from 'fs/promises'
-import map from 'vinyl-map2'
+import rewriteImports from 'gulp-rewrite-imports'
+import path from 'path'
 
 /** @type {{version:string}} */
 const pj = await fsa.readFile("package.json").then(jsonString => JSON.parse(jsonString));
+const unpkgRoot = "https://unpkg.com/microdata-tooling@" + pj.version + "/dist/";
+/** @type {{[k:string]: string}} */
+const importMappings = {};
+for (const srcFile of await fsa.readdir("src/")) {
+    const name = path.basename(srcFile, ".ts");
+    importMappings["./" + name + ".js"] = unpkgRoot + name + ".min.js";
+}
 
 gulp.task("docs", function () {
     return gulp.src("src/index.ts").pipe(typedoc({
@@ -40,22 +48,11 @@ gulp.task("ts-build", function () {
 gulp.task("minify", function () {
     return gulp.src("dist/*.js").
         pipe(sourcemaps.init()).
-        pipe(map(/** @param {string | Buffer} code @param {string} filename @returns {string | null} */(code, filename) => {
-            code = code.toString();
-            // Yeah... not a great idea but it works ¯\_(ツ)_/¯
-            const rewrite = {
-                "./apply.js": "https://unpkg.com/browse/microdata-tooling@" + pj.version + "/dist/apply.min.js",
-                "./errors.js": "https://unpkg.com/browse/microdata-tooling@" + pj.version + "/dist/errors.min.js",
-                "./jsonify.js": "https://unpkg.com/browse/microdata-tooling@" + pj.version + "/dist/jsonify.min.js",
-                "./parse.js": "https://unpkg.com/browse/microdata-tooling@" + pj.version + "/dist/parse.min.js",
-                "./schema-simple.js": "https://unpkg.com/browse/microdata-tooling@" + pj.version + "/dist/schema-simple.min.js",
-                "./sort-nodes.js": "https://unpkg.com/browse/microdata-tooling@" + pj.version + "/dist/sort-nodes.min.js",
-                "./utils.js": "https://unpkg.com/browse/microdata-tooling@" + pj.version + "/dist/utils.min.js"
-            };
-            for (const imp in rewrite) {
-                code = code.replace(imp, rewrite[imp]);
-            }
-            return code;
+        pipe(rewriteImports({
+            noRequire: true,
+            noImport: true,
+            experimentalEnableStreams: true,
+            mappings: importMappings
         })).
         pipe(terser()).
         pipe(rename({ extname: ".min.js" })).
